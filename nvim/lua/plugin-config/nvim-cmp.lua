4292acf23bef04d0812c1dbf8716772dd73d9a03
Jasper <[jasper.vandervoo@akqa.com](mailto:jasper.vandervoo@akqa.com)>
2023-12-20 15:05:49 +1300

    Disable completion in comments. Disable snippets.

--- nvim/lua/plugin-config/nvim-cmp.lua
+++ nvim/lua/plugin-config/nvim-cmp.lua
@@ -1,6 +1,6 @@
-vim.o.completeopt="menu,menuone,noselect"
+vim.o.completeopt = "menu,menuone,noselect"
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
@@ -12,11 +12,11 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ 
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
             select = true,
             behaviour = cmp.ConfirmBehavior.Insert,
         }),
@@ -41,13 +41,24 @@ cmp.setup({
         end, { "i", "s" }),
     },
     sources = cmp.config.sources({
-        { name = 'nvim_lsp', priority = 1 },
-        { name = 'vsnip' }, -- For vsnip users.
+        { name = "nvim_lsp", priority = 1 },
+        -- { name = 'vsnip' }, -- For vsnip users.
         -- { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
     }, {
-        { name = 'buffer' },
-    })
+        { name = "buffer" },
+    }),
 })
-
+cmp.setup({
+    enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+            return true
+        else
+            return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+        end
+    end,
+})
