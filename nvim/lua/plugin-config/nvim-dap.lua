828986383ee45292a9b21227bbdb38bc6a291bb3
Jasper van der Voo <jasper.vandervoo@akqa.com>
2025-04-04 08:57:22 +1300

    Add second php xdebug config to dap

--- nvim/lua/plugin-config/nvim-dap.lua
+++ nvim/lua/plugin-config/nvim-dap.lua
@@ -1,10 +1,10 @@
-local dap = require('dap')
+local dap = require("dap")
 dap.adapters.php = {
-    type = 'executable',
-    command = 'node',
-    -- you need to clone vscode php debug repo in your home directory
-    -- in order for the adapter to be available
-    args = { os.getenv('HOME') .. '/vscode-php-debug/out/phpDebug.js' }
+	type = "executable",
+	command = "node",
+	-- you need to clone vscode php debug repo in your home directory
+	-- in order for the adapter to be available
+	args = { os.getenv("HOME") .. "/vscode-php-debug/out/phpDebug.js" },
 }
 
 -- grab the current directory to map for debugging
@@ -14,16 +14,28 @@ local cd = string.gsub(handle:read("*a"), "\n", "")
 handle:close()
 
 dap.configurations.php = {
-    {
-        type = 'php',
-        request = 'launch',
-        name = 'Listen for Xdebug',
-        port = 9000,
-        xdebugSettings = {
-            max_children = 999
-        },
-        pathMappings = {
-            ["/var/www/html"] = cd
-        }
-    }
+	{
+		type = "php",
+		request = "launch",
+		name = "Port 9000",
+		port = 9000,
+		xdebugSettings = {
+			max_children = 999,
+		},
+		pathMappings = {
+			["/var/www/html"] = cd,
+		},
+	},
+	{
+		type = "php",
+		request = "launch",
+		name = "Port 9003",
+		port = 9003,
+		xdebugSettings = {
+			max_children = 999,
+		},
+		pathMappings = {
+			["/var/www/html"] = cd,
+		},
+	},
 }
